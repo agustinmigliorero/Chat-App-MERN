cf4c3449b04aa78ddb4409f77bd4c7c5fbedc465
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-13 08:17:25 -0300

    Componente de conversaciones y hook de enviar mensajes

--- cliente/src/components/Sidebar/Conversation.jsx
+++ cliente/src/components/Sidebar/Conversation.jsx
@@ -1,25 +1,32 @@
-function Conversation() {
+import useConversation from "../../zustand/useConversation";
+
+function Conversation({ conversation, emoji, lastIdx }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = conversation._id === selectedConversation?._id;
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${
+          isSelected ? "bg-sky-500" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img
-              src="https://avatar.iran.liara.run/public/40"
-              alt="Imagen de usuario"
-            />
+            <img src={conversation.profilePic} alt="Imagen de usuario" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">Nombre Apellido</p>
-            <span className="text-xl">😎</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
 
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 }
