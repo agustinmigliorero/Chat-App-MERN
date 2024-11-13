75e31597d804f3c0e19fb35355025db2cf4b5c0f
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-13 09:21:02 -0300

    Componente de buscar una conversacion

--- cliente/src/components/Sidebar/SearchInput.jsx
+++ cliente/src/components/Sidebar/SearchInput.jsx
@@ -1,11 +1,36 @@
 import { IoSearchSharp } from "react-icons/io5";
+import { useState } from "react";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { toast } from "react-hot-toast";
 
 function SearchInput() {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No se encontró ninguna conversación");
+    }
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <input
         type="text"
         placeholder="Buscar..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         className="input input-bordered rounded-full bg-gray-800 text-gray-200"
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
