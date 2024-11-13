3ece889826f990cbe6f2dd37ca2fd45a772b4ffe
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-13 02:24:22 -0300

    Controlador para crear un mensaje y que se asocie a la conversacion con sus dos usuarios

--- servidor/controllers/messageControllers.js
+++ servidor/controllers/messageControllers.js
@@ -1,9 +1,39 @@
+import Conversation from "../models/conversationModel.js";
+import Message from "../models/messageModel.js";
+
 const sendMessage = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id: receiverId } = req.params;
     const { message } = req.body;
-
     const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = new Conversation({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage);
+    }
+
+    //ACA VA A IR LA FUNCIONALIDAD CON WEBSOCKETS
+
+    //ACA VA A IR LA FUNCIONALIDAD CON WEBSOCKETS
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error en el controlador de enviar mensaje", error.message);
     res
